0d3b57d438f3c5e10f3d80003b9b92f67f9b9070
Jorge <jotagmzn@gmail.com>
2018-08-27 14:42:21 +0200

    added typed and particles functionality

--- src/app/index.js
+++ src/app/index.js
@@ -1,2 +1,21 @@
+//JS scripts
+import Typed from 'typed.js';
+import 'particles.js';
+
 // Styles 
 import '../styles/scss/main.scss';
+
+// HEADER CONTROLLER
+
+const typed = new Typed('#jotagep', {
+    showCursor: false,
+    typeSpeed: 40,
+    backSpeed: 40,
+    loop: true,
+    backDelay: 5000,
+    strings: ['jotagep', 'Jorge G. Palacin']
+});
+
+particlesJS.load('particles', 'assets/particlesjs-config.json', () => {
+    console.log('Particle JS - loaded');
+})
