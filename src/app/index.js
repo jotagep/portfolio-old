450af9e82084647ce66cffe69e3b0553cc132d8b
Jorge <jotagmzn@gmail.com>
2018-08-29 14:11:23 +0200

    Add navigation__items scroll

--- src/app/index.js
+++ src/app/index.js
@@ -4,7 +4,9 @@ import 'particles.js';
 import 'waypoints/lib/noframework.waypoints';
 
 // DOM elements
-import { elements } from './views/base';
+import {
+    elements
+} from './views/base';
 
 // Styles 
 import '../styles/scss/main.scss';
@@ -12,14 +14,29 @@ import '../styles/scss/main.scss';
 
 // NAVIGATION CONTROLLER
 
-elements.closeNav.addEventListener('click', () => {
+const closeNav = () => {
     elements.nav.classList.remove('open');
+    document.body.style.overflowY = 'auto';
+}
+
+elements.closeNav.addEventListener('click', () => {
+    closeNav();
+});
+
+elements.nav_items.forEach( el => {
+    el.addEventListener('click', () => {
+        closeNav();
+        window.scrollTo({
+            top: document.querySelector(`${el.dataset.dest}`).offsetTop
+        })
+    })
 });
 
 // HEADER CONTROLLER
 
 elements.openNav.addEventListener('click', () => {
-    elements.nav.classList.add('open')
+    elements.nav.classList.add('open');
+    document.body.style.overflowY = 'hidden';
 })
 
 const typed = new Typed('#jotagep', {
@@ -35,15 +52,47 @@ particlesJS.load('particles', 'assets/particlesjs-config.json', () => {
     console.log('Particle JS - loaded');
 })
 
+elements.header_btn.addEventListener('click', () => {
+    window.scrollTo({
+        top: elements.section_projects.offsetTop
+    })
+})
+
 //NAVBAR
 
-document.addEventListener('DOMContentLoaded', () => {
-    //
-})
+// document.addEventListener('DOMContentLoaded', () => {
+//    
+// })
 
-const waypoint = new Waypoint({
+const navbar_wp = new Waypoint({
     element: elements.navbar,
-    handler: function(direction) {
-      direction == 'up' ? this.element.classList.remove('navbar--sticky') : this.element.classList.add('navbar--sticky');
+    handler: function (direction) {
+        if (direction == 'up') {
+            this.element.classList.remove('navbar--sticky');
+            elements.section_about.style.marginTop = `0px`;
+        } else {
+            this.element.classList.add('navbar--sticky');
+            elements.section_about.style.marginTop = `60px`;
+        }
     }
 })
+
+const removeActive = () => {
+    document.querySelectorAll('.navbar__item--active').forEach((el) => {
+        el.classList.remove('navbar__item--active');
+    })
+}
+
+// ABOUT
+
+const about_wp = new Waypoint({
+    element: elements.section_about,
+    handler: function (direction) {
+        if (direction == 'down') {
+            removeActive();
+            const item = document.querySelector('#about');
+            item.classList.add('navbar__item--active');
+            console.log(item);
+        }
+    }
+});
--- src/app/views/base.js
+++ src/app/views/base.js
@@ -2,6 +2,10 @@
 export const elements = {
     closeNav: document.querySelector('.navigation__close'),
     openNav: document.querySelector('.header__menu'),
+    header_btn: document.querySelector('.header__btn'),
     nav: document.querySelector('.navigation'),
-    navbar: document.querySelector('#navbar')
+    nav_items: document.querySelectorAll('.navigation__item'),
+    navbar: document.querySelector('#navbar'),
+    section_about: document.querySelector('#section-about'),
+    section_projects: document.querySelector('#section-projects'),
 }
